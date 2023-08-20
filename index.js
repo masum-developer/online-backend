e1983f64a2c5fa3be58086ecde766565af48e9b7
masumbillahwebdeveloper <masumbillah.webdeveloper@gmail.com>
2023-08-20 17:31:44 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const http= require('http');
+const {Server} = require('socket.io');
+const server = http.createServer(app);
+
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+const io = new Server(server);
+
+
+
+const userSocketMap = {};
+const ACTIONS = {
+    JOIN:'join',
+    JOINED:'joined',
+    DISCONNECTED:'disconnected',
+    CODE_CHANGE:'code-change',
+    SYNC_CODE: 'sync-code',
+    LEAVE: 'leave',
+}
+
+function getAllConnectedClients(roomId){
+	return Array.from(io.sockets.adapter.rooms.get(roomId) || []).map((socketId)=>{
+		return {
+			socketId,
+			username:userSocketMap[socketId],
+		}
+	});
+}
+
+io.on('connection',(socket)=>{
+    console.log('socket connected' ,socket.id);
+    socket.on(ACTIONS.JOIN,({roomId,username})=>{
+    	userSocketMap[socket.id] = username;
+    	socket.join(roomId);
+    	const clients = getAllConnectedClients(roomId);
+    	console.log(clients);
+    	clients.forEach(({socketId})=>{
+    		io.to(socketId).emit(ACTIONS.JOINED,{
+    			clients,
+    			username,
+    			socketId:socket.id
+    		})
+    	})
+    })
+
+	socket.on(ACTIONS.CODE_CHANGE,({roomId,code})=>{
+		
+		socket.in(roomId).emit(ACTIONS.CODE_CHANGE,{code});
+	})
+	socket.on(ACTIONS.SYNC_CODE,({socketId,code})=>{
+		
+		io.to(socketId).emit(ACTIONS.CODE_CHANGE,{code});
+	})
+	socket.on('disconnecting',()=>{
+		const rooms = [...socket.rooms];
+		rooms.forEach((roomId)=>{
+			socket.in(roomId).emit(ACTIONS.DISCONNECTED,{
+				socketId:socket.id,
+				username:userSocketMap[socket.id],
+			})
+
+		})
+		delete userSocketMap[socket.id];
+		socket.leave();
+	})
+})
+
+
+app.get('/', (req,res)=>{
+    res.send('Online Code Editor is Running');
+})
+
+server.listen(port,()=>console.log(`Listen on port ${port}`));
